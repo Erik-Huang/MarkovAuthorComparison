e7cd1d9383f917ae52d51254249f598f9489ac2b
Erik Huang <huangti@uw.edu>
2020-03-01 16:45:54 -0800

    Generated composite matrices

--- markovAll.py
+++ markovAll.py
@@ -18,18 +18,27 @@ AUTHOR_LIST = [
 ROWS, COLS = (28, 28)
 
 def main():
-    resultList = open("./matricesPathList", "w+")
+    matricesPathList = open("./matricesPathList.txt", "w+")
+    genAllMatrices(matricesPathList)
+    matricesPathList.close()
+
+# Generate matrices for each book and all books for an author
+def genAllMatrices(matricesPathList):
     for author in AUTHOR_LIST:
         print("Started on", author)
+        fullText = "";
         fileList = getAllBooksForAuthor(author)
+        folderDirectory = fileList[0].split("/")[1] + " Matrices"
+        os.system("mkdir " + folderDirectory.replace(" ", "\ "))
         for filePath in fileList:
             book = open(filePath, "r")
             text = book.read().lower()
+            fullText += text
             book.close()
             T = getTransMatrix(text, filePath)
-            createCSV(filePath, T, resultList)
-    resultList.close()
-
+            createCSV(filePath, T, matricesPathList)
+        T = getTransMatrix(fullText, filePath)
+        createCSV(filePath, T, matricesPathList, True)
 
 def getTransMatrix(text, filePath):
     T = [[0 for r in range(COLS)] for c in range(ROWS)]
@@ -67,19 +76,22 @@ def reverseName(author):
     author = author.split(", ")
     return author[1]+" "+author[0]
 
-def createCSV(filePath, T, resultList):
+# File path here is used to locate the folder directory and create
+# abbrivation for books
+def createCSV(filePath, T, matricesPathList, isComposite = False):
     filePath = filePath.split("/")
-    mtxDirPath = filePath[1] + " Matrices"
-    os.system("mkdir " + mtxDirPath.replace(" ", "\ "))
-    outputPath = "./%s/%s" % (mtxDirPath, filePath[1].replace(" ", ""))
-    bookName = filePath[2].split(" ")
-    for word in bookName:
-        outputPath += word[0].upper()
+    outputPath = "./%s/%s" % (filePath[1]+" Matrices", filePath[1].replace(" ", ""))
+    if isComposite:
+        outputPath += "Composite"
+    else:
+        bookName = filePath[2].split(" ")
+        for word in bookName:
+            outputPath += word[0].upper()
     outputPath += ".csv"
-    print("saving matrix result of", filePath[2])
+    print("saving matrix result to", outputPath)
     df = pd.DataFrame(data=T, columns=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', "space", "symbol"])
     df.to_csv(outputPath, index=False, encoding='utf8')
-    resultList.write(outputPath + "\n")
+    matricesPathList.write(outputPath + "\n")
 
 if __name__ == '__main__':
     main()
